aebaa1bd98aa370bd50d9fca6b2154c9ffb6159e
Andreas Smas <andreas@lonelycoder.com>
2021-07-12 07:48:40 +0200

    Add some helpers for detecting opengl errors

--- opengl.hpp
+++ opengl.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <assert.h>
+
 #include <GL/glew.h>
 
 #define IMGUI_DEFINE_MATH_OPERATORS
@@ -8,3 +10,13 @@
 #include "imgui_impl_opengl3.h"
 
 #include <GLFW/glfw3.h>
+
+
+namespace g3d {
+
+GLenum checkGlError_(const char *file, int line);
+}
+
+#define checkGlError() checkGlError_(__FILE__, __LINE__)
+
+#define assertGlError() assert(glGetError() == GL_NO_ERROR)
