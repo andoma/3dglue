a26920a61755c2143a9d1d21741c3c4c7b9c3840
Andreas Smas <andreas@lonelycoder.com>
2021-08-10 11:35:18 +0200

    Fix ignored warning on clang

--- texture.cpp
+++ texture.cpp
@@ -4,7 +4,9 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #pragma GCC diagnostic ignored "-Wsign-compare"
+#ifndef __clang__
 #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#endif
 #include "stb_image.h"
 
 namespace g3d {
