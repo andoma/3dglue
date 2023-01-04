0a316e59b7e1320a68cd2f1990b68e32bb5dbd11
Andreas Smas <andreas@lonelycoder.com>
2023-01-04 11:52:11 +0100

    simd.hpp: On clang, use builtins

--- simd.hpp
+++ simd.hpp
@@ -2,6 +2,45 @@
 
 #include <glm/glm.hpp>
 
+#ifdef __clang__
+
+namespace g3d {
+
+typedef float float4 __attribute__((vector_size(16)));
+
+float4
+min(float4 a, float4 b)
+{
+    return __builtin_elementwise_min(a, b);
+}
+
+float4
+max(float4 a, float4 b)
+{
+    return __builtin_elementwise_max(a, b);
+}
+
+float4
+min(float4 a, float4 b, float4 c)
+{
+    return __builtin_elementwise_min(__builtin_elementwise_min(a, b), c);
+}
+
+float4
+max(float4 a, float4 b, float4 c)
+{
+    return __builtin_elementwise_max(__builtin_elementwise_max(a, b), c);
+}
+
+float4
+from(const glm::vec3 &v)
+{
+    return {v.x, v.y, v.z, 0};
+}
+};
+
+#else
+
 #include <xmmintrin.h>
 
 namespace g3d {
@@ -39,3 +78,5 @@ from(const glm::vec3 &v)
 }
 
 }  // namespace g3d
+
+#endif
