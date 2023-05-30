5572e21b0c0b5647dc7235318b07a63cbe183e8d
Andreas Smas <andreas@lonelycoder.com>
2023-05-30 16:21:21 +0200

    loadOBJ(): Return indices wrapped in a shared_ptr

--- loader.cpp
+++ loader.cpp
@@ -7,7 +7,8 @@
 
 namespace g3d {
 
-std::pair<std::shared_ptr<VertexBuffer>, std::vector<glm::ivec3>>
+std::pair<std::shared_ptr<VertexBuffer>,
+          std::shared_ptr<std::vector<glm::ivec3>>>
 loadOBJ(const char *path, const glm::mat4 transform)
 {
     FILE *fp = fopen(path, "r");
@@ -15,7 +16,7 @@ loadOBJ(const char *path, const glm::mat4 transform)
         throw std::system_error(errno, std::system_category());
 
     std::vector<glm::vec3> vertices;
-    std::vector<glm::ivec3> triangles;
+    auto triangles = std::make_shared<std::vector<glm::ivec3>>();
 
     while(!feof(fp)) {
         double a, b, c;
@@ -25,7 +26,7 @@ loadOBJ(const char *path, const glm::mat4 transform)
                 auto v = transform * glm::vec4{a, b, c, 1};
                 vertices.push_back(glm::vec3{v});
             } else if(x == 'f') {
-                triangles.push_back(glm::ivec3{a - 1, b - 1, c - 1});
+                triangles->push_back(glm::ivec3{a - 1, b - 1, c - 1});
             }
         }
     }
